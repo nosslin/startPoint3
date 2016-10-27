b82e3e72d5f3ba0dabc9ccb5a69108b2740260e7
Peter <peter@nosslin.net>
2016-10-28 00:19:45 +0200

    Just an standard template solution

---
+++ startPoint3/src/startPoint3/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace startPoint3.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
